6e1f211acac78d9efe4c0589955e9ca05657f911
Manoj <pmontu@gmail.com>
2013-01-25 19:04:21 +0530

    maybe go with fixed big resolution
    unable to change resolution during run time
    will wpfmedia kit make any resolution work on any device?
    
    an alternative
    http://sumitmaitra.wordpress.com/2011/03/13/capturing-images-through-webcams-in-a-wpf-app/
    
    btw unable to make combobox databinding with object work unless
    properties get and set are defined!

--- mediakit1/mediakit1/MainWindow.xaml.cs
+++ mediakit1/mediakit1/MainWindow.xaml.cs
@@ -28,7 +28,7 @@ namespace mediakit1
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            RenderTargetBitmap bmp = new RenderTargetBitmap(320, 240, 96, 96, PixelFormats.Pbgra32);
+            RenderTargetBitmap bmp = new RenderTargetBitmap(1280, 720, 96, 96, PixelFormats.Pbgra32);
             bmp.Render(videoCapElement);
             BitmapEncoder encoder = new JpegBitmapEncoder();
             encoder.Frames.Add(BitmapFrame.Create(bmp));
@@ -54,6 +54,37 @@ namespace mediakit1
         private void Window_Activated_1(object sender, EventArgs e)
         {
             videoCapDevices.SelectedIndex = 0;
+        }
+
+        private void cbxResolution_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<resolution> resolutions = new List<resolution>();
+            resolutions.Add(new resolution("320x280", 320, 280));
+            resolutions.Add(new resolution("1280x720", 1280, 720));
+            cbxResolution.ItemsSource = resolutions;
+            cbxResolution.DisplayMemberPath = "Name";
+            cbxResolution.SelectedIndex = 1;
+        }
+
+        private void cbxResolution_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cbxResolution.SelectedIndex != -1)
+            {
+                Point r = ((resolution)cbxResolution.SelectedValue).value;
+                //change the capture resolution here.
+            }
+        }
+    }
+    public class resolution
+    {
+        public string Name { get; set; }
+        public Point value;
+
+        public resolution(string p1, int p2, int p3)
+        {
+            // TODO: Complete member initialization
+            this.Name = p1;
+            this.value = new Point(p2, p3);
         }
     }
 }
