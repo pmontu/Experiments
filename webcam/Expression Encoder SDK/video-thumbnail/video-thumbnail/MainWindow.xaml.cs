799d940957e3cad7b1ffe3e7c7d93e86d80b0907
Manoj <pmontu@gmail.com>
2013-01-31 20:26:34 +0530

    video thumbnail using expression encoder
    * object instanstiation takes a long pause
    *
    http://stackoverflow.com/questions/2840857/generating-thumbnail-image-for-video
    * bitmap needs to be saved and converted to image source to be displayed
    by image control -
    http://stackoverflow.com/questions/1118496/using-image-control-in-wpf-to-display-system-drawing-bitmap

---
+++ webcam/Expression Encoder SDK/video-thumbnail/video-thumbnail/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Expression.Encoder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace video_thumbnail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            var path = @"C:\Users\ManojKumar\Documents\GitHub\PatientRecordsWindows\PatientRecordsWPF2\bin\Debug\Media\2013Jan31-195929-Thu.wmv";
+
+            var video = new MediaItem(path);
+            using (var bitmap = video.MainMediaFile.GetThumbnail(
+                new TimeSpan(0, 0, 1),
+                new System.Drawing.Size(30, 30)))
+            {
+                // do something with the bitmap like:
+                var bpath = path + ".thumbnail.jpg";
+                bitmap.Save(bpath);
+                img.Source = new BitmapImage(new Uri(bpath));
+            }
+        }
+    }
+}
