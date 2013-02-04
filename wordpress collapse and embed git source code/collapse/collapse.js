10ba30e6bc606216c72992844e36b61526510c37
Manoj <pmontu@gmail.com>
2013-02-04 19:29:25 +0530

    Shortcode

--- wordpress collapse and embed git source code/collapse/collapse.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function ($) {
-  $("pre").html("working");
-});
--- wordpress collapse and embed git source code/collapse/collapse.php
+++ wordpress collapse and embed git source code/collapse/collapse.php
@@ -11,35 +11,15 @@ Version: 0
 Author URI: http://pmontu.com/
 */
 
-// This just replaces the bad word with censored, we'll style it later
-function collapse($content) {
-    $profanities = array('badword','alsobad','...');
-    $content=str_ireplace($profanities,'{censored}',$content);
-    return $content;
-}
-
-// Now we set that function up to execute when the the_content filter is called
-add_filter( 'the_content', 'collapse' );
-
-// We need some CSS to position the paragraph
-function collapse_css() {
-
-	echo "
-	<style type='text/css'>
-	#dolly {
+// [bartag foo="foo-value"]
+function github_func( $atts ) {
+	extract($atts);
+	$url = trim($file);
+	$raw = str_replace(array("https://","/blob/"),array("https://raw.","/"),$url);
+	if (strlen($raw) > 0){
+		return file_get_contents($raw)."<br><a href='$url'>$name</a>";
 	}
-	</style>
-	";
 }
+add_shortcode( 'github', 'github_func' );
 
-add_action( 'admin_head', 'collapse_css' );
-
-function my_scripts_method() {
-	wp_enqueue_script(
-		'collapse',
-		plugins_url('collapse.js',__FILE__),
-		array('jquery')
-	);
-}
-add_action('wp_enqueue_scripts', 'my_scripts_method');
 ?>
