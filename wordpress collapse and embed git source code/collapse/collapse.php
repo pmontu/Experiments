1b9c646b663c972f35491448fad6f069170cf4bf
Manoj <pmontu@gmail.com>
2013-02-05 15:16:27 +0530

    Collapse

--- wordpress collapse and embed git source code/collapse/collapse.php
+++ wordpress collapse and embed git source code/collapse/collapse.php
@@ -23,4 +23,35 @@ function github_func( $atts ) {
 	}
 }
 add_shortcode( 'github', 'github_func' );
+
+function collapse_func( $atts, $contents ) {
+	extract($atts);
+	return "<a href='' class='collapsible'>$name<span></span></a>"
+	."<div>"
+	.do_shortcode($contents)
+	."</div>";
+}
+add_shortcode( 'collapse', 'collapse_func' );
+
+function my_scripts_method() {
+	wp_enqueue_script(
+		'collapse',
+		plugins_url('collapse.js',__FILE__),
+		array('jquery')
+	);
+	wp_enqueue_script(
+		'jquery.collapsible',
+		plugins_url('jquery.collapsible.js',__FILE__),
+		array('jquery')
+	);
+	wp_register_style( 
+		'collapse_css', 
+		plugins_url( 'collapse.css', __FILE__ ),
+		array(),
+		'0',
+		'all'
+	);
+	wp_enqueue_style( 'collapse_css');
+ }
+add_action('wp_enqueue_scripts', 'my_scripts_method');
 ?>
