93ba26a52565fd2ef122e1587ec1f6a6b8772f98
Manoj <pmontu@gmail.com>
2013-02-15 13:23:03 +0530

    MVVM Demo
    model - a number set to 0
    viewmodel - number from model, up command: increment number untill 5
    view - datacontenxt set to viewmodel, label bound to number, button
    command set to up
    
    ref:
    * source -
    http://www.codeproject.com/script/articles/download.aspx?file=/KB/WPF/435478/MVVMSample.zip&rp=http://www.codeproject.com/Articles/435478/MVVM-Demo
    * video tutorial -
    http://www.lab49.com/wp-content/uploads/2011/12/Jason_Dolinger_MVVM.wmv
    * lambda -
    http://mfelicio.com/2010/01/10/safe-usage-of-inotifypropertychanged-in-mvvm-scenarios/

---
+++ Mvvm/WpfApplication1/ViewModels/DemoViewModel.cs
@@ -0,0 +1,66 @@
+﻿using MVVMSample.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApplication1.ViewModels
+{
+    class DemoViewModel : INotifyPropertyChanged
+    {
+        private readonly Models.Demo domObject;
+        private readonly ICommand upCmd;
+
+        public DemoViewModel()
+        {
+            domObject = new Models.Demo();
+            upCmd = new RelayCommand(Up, CanUp);
+        }
+
+        public int Number
+        {
+            get { return domObject.Number; }
+            set
+            {
+                domObject.Number = value;
+                RaisePropertyChanged(() => this.Number);
+            }
+        }
+
+        private bool CanUp(object obj)
+        {
+            if(Number<5)
+                return true;
+            return false;
+        }
+
+        private void Up(object obj)
+        {
+            Number += 1;
+        }
+
+        public ICommand UpCmd { get { return upCmd; } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            //Fire the PropertyChanged event in case somebody subscribed to it
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression.Body.NodeType == ExpressionType.MemberAccess)
+            {
+                var memberExpr = propertyExpression.Body as MemberExpression;
+                string propertyName = memberExpr.Member.Name;
+                this.RaisePropertyChanged(propertyName);
+            }
+        }
+    }
+}
